5f8f59af5acdc4b326ddb90588e90857381df07d
Admin <huynhgiavinh4905@gmail.com>
2025-04-09 08:19:49 +0700

    First Demo Test Pass

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Calculator implements Icalculator {
     public Integer sum(Integer a, Integer b){
-        return 0;
+        return a+b;
     }
 
     public Integer sub(Integer a, Integer b){
-        return 0;
+        return a-b;
     }
 }
